00ec4f8fc1fa797e7ce2218c9dfdaa232cd47f35
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-09-19 10:23:22 -0300

    iniciando main

--- aula-react-02/src/components/Main.jsx
+++ aula-react-02/src/components/Main.jsx
@@ -1,6 +1,15 @@
 function Main(){
     return(
-        
+        <main className="">
+            <section className="sectionMain">
+                <h2>Nossas Unidades</h2>
+                <p>Av Paulista, 1106</p>
+            </section>
+            <section className="sectionMain">
+                <h2>Contato</h2>
+                <p>(11)98248-6626</p>
+            </section>
+        </main>
     )
 }
 export default Main
